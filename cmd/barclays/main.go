f2c52d1b17973abb389be316d55697400febdcad
George Garside <apps@georgegarside.com>
2025-04-08 14:14:37 +0100

    Skip balance forward lines
    Lines without change (payment or receipt) but containing date are repeated balance lines which don't need outputting.

--- cmd/barclays/main.go
+++ cmd/barclays/main.go
@@ -82,7 +82,9 @@ func parse(f io.Reader, w *csv.Writer) error {
 
 		details, payments, receipts, date, running := strings.TrimSpace(record[0]), strings.TrimSpace(record[1]), strings.TrimRight(record[2], "C "), strings.TrimSpace(record[3]), strings.TrimSpace(record[4])
 		if payments == "" && receipts == "" {
-			desc = details
+			if date == "" {
+				desc = details
+			}
 			continue
 		}
 		if payments != "" {
