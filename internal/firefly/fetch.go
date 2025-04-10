98ae8792c6c93c574a93fb0f0fea6094a0712403
George Garside <apps@georgegarside.com>
2025-04-10 15:53:30 +0100

    Support query parameters in fetch command

--- internal/firefly/fetch.go
+++ internal/firefly/fetch.go
@@ -2,18 +2,24 @@ package firefly
 
 import (
 	"context"
+	"net/url"
 	"os"
 
 	"github.com/go-json-experiment/json"
 )
 
 type Fetch struct {
-	Path string `arg:""`
+	Path  string            `arg:""`
+	Query map[string]string `short:"q"`
 }
 
 func (f Fetch) Run(ctx context.Context, a API) error {
+	q := make(url.Values, len(f.Query))
+	for k, v := range f.Query {
+		q.Add(k, v)
+	}
 	var resp any
-	if err := Do(ctx, a, f.Path, nil, &resp, nil); err != nil {
+	if err := Do(ctx, a, f.Path, q, &resp, nil); err != nil {
 		return err
 	}
 	return json.MarshalWrite(os.Stdout, resp)
